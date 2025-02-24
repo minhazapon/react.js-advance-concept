733a5893dbb8f8ba4c2e336835f43943ada86a52
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-24 01:45:22 -0800

    added file

--- src/useId/UseId.jsx
+++ src/useId/UseId.jsx
@@ -2,8 +2,24 @@
 
 function UseId() {
     return (
-        <div>
+        <div className=" m-20 flex justify-center ">
+            <div>
+                <p className=" text-center font-bold text-5xl text-black ">useID</p>
 
+                <div className=" mt-10 ">
+                    <form>
+
+                        <input required className=" p-3 h-[50px] w-[600px] border-[1px]" placeholder="Name" name="number" type="number" />
+                        <br></br>
+                        <input required className=" p-3 h-[50px] w-[600px] border-[1px]" placeholder="password" name="password" type="password" />
+                        <br></br>
+                        <input className=" btn p-3 h-[50px] w-[600px] bg-black text-white rounded-none " type="submit" value="Submit" />
+                        <br></br>
+
+                    </form>
+                </div>
+
+            </div>
         </div>
     )
 }
