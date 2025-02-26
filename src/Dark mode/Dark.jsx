7f731f601a4b27fc4ec0204098a484cbea964605
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-26 02:07:06 -0800

    added file

--- src/Dark mode/Dark.jsx
+++ src/Dark mode/Dark.jsx
@@ -6,12 +6,11 @@ function Dark() {
 
     const { darkMode, handleDarkMode } = use(ThemeContext)
 
-
     return (
 
         <ThemeProvider>
 
-            <div className={` h-screen ${darkMode === "dark" ? "bg-black" : "bg-white"} p-20 `}>
+            <div className={` h-screen ${darkMode === "dark" ? "bg-gray-900" : "bg-white"} p-20 `}>
                 <div className=" ">
                     <p className={`${darkMode === "light" ? "text-black" : "text-white"} text-center text-6xl font-bold  text-black`}> The Dark and Light Mode </p>
                     <div className=" flex justify-center ">
