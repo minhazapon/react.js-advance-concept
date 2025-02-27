4ee69b24d3a49329fca4fb4ff94b5a3849051e3e
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-27 01:51:01 -0800

    added file

--- src/use memo/UM.jsx
+++ src/use memo/UM.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react"
 
 
 function UM() {
+
+    const [count, setCount] = useState(0)
+    const [number, setNumber] = useState(10)
+
+
     return (
-        <div>
+        <div className=" m-20 ">
+
+            <p className=" text-center text-4xl font-bold text-black ">Increase Performance: </p>
+
+            <div className=" flex justify-center items-center gap-5 mt-10 ">
+                <button className=" btn w-[300px] bg-black border-cyan-400 text-white ">
+                    Increase count:  </button>
+                <button className=" btn w-[300px] bg-black border-cyan-400 text-white ">
+                    Increase number:   </button>
+            </div>
 
         </div>
     )
