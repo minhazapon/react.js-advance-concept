b096f5360d52f1cf25c54fa65722a9b6beb70542
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-26 20:33:16 -0800

    added file

--- src/use reducer/Reducer.jsx
+++ src/use reducer/Reducer.jsx
@@ -1,10 +1,21 @@
 
 
 function Reducer() {
-    return (
-        <div>
 
-        </div>
+
+
+    return (
+        <>
+            <div className=" m-20 ">
+
+                <div className=" flex justify-center items-center gap-5 ">
+                    <button className=" btn w-[400px] bg-black text-white ">Increment</button>
+                    <p>count</p>
+                    <button className=" btn w-[400px] bg-black text-white">Decrement</button>
+                </div>
+
+            </div>
+        </>
     )
 }
 
