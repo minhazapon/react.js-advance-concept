d6f9bf4c84d72f5c63adf6165aca0fa73fe4ce0a
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-27 02:06:08 -0800

    added file

--- src/use memo/UM.jsx
+++ src/use memo/UM.jsx
@@ -1,25 +1,36 @@
+import { useMemo } from "react"
 import { useState } from "react"
-
 
 function UM() {
 
     const [count, setCount] = useState(0)
     const [number, setNumber] = useState(10)
 
+    const memozation = useMemo(() => {
+
+        let value = 0
+        for (let i = 0; i < 10000; i++) {
+            value += number
+        }
+        return value
+
+    }, [number])
+
 
     return (
-        <div className=" m-20 ">
-
-            <p className=" text-center text-4xl font-bold text-black ">Increase Performance: </p>
-
-            <div className=" flex justify-center items-center gap-5 mt-10 ">
-                <button className=" btn w-[300px] bg-black border-cyan-400 text-white ">
-                    Increase count:  </button>
-                <button className=" btn w-[300px] bg-black border-cyan-400 text-white ">
-                    Increase number:   </button>
-            </div>
-
-        </div>
+        <>
+            <div className=" m-20 ">
+                <p className=" text-center text-4xl font-bold text-black ">Increase Performance: {memozation} </p>
+                <div className=" flex-col md:flex-row lg:flex-row- flex justify-center items-center gap-5 mt-10 ">
+                    <button onClick={() => setCount(count + 1)} className=" btn w-[300px] bg-black border-cyan-400 text-white ">
+                        Increase count: {count}  </button>
+                    <button onClick={() => setNumber(number + 1)} className=" btn w-[300px] bg-black border-cyan-400 text-white ">
+                        Increase number: {number}  </button>
+                    <button onClick={() => setNumber(number === "")} className=" btn w-[300px] bg-black border-cyan-400 text-white ">
+                        reset  </button>
+                </div>
+            </div >
+        </>
     )
 }
 
