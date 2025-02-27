56125eef7e76d6c77f7fc59f43b67eec1b54fa14
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-26 20:40:23 -0800

    added file

--- src/use reducer/Reducer.jsx
+++ src/use reducer/Reducer.jsx
@@ -1,19 +1,28 @@
+import { useReducer } from "react"
 
 
 function Reducer() {
 
+    const reducer = (state, action) => {
 
+        if (action.type === "Increment") {
+            return state + 1
+        }
+        if (action.type === "Decrement") {
+            return state - 1
+        }
+    }
+
+    const [count, dispatch] = useReducer(reducer, 0)
 
     return (
         <>
             <div className=" m-20 ">
-
                 <div className=" flex justify-center items-center gap-5 ">
-                    <button className=" btn w-[400px] bg-black text-white ">Increment</button>
-                    <p>count</p>
-                    <button className=" btn w-[400px] bg-black text-white">Decrement</button>
+                    <button onClick={() => dispatch({ type: "Increment" })} className=" btn w-[400px] bg-black text-white ">Increment</button>
+                    <p> {count} </p>
+                    <button onClick={() => dispatch({ type: "Decrement" })} className=" btn w-[400px] bg-black text-white">Decrement</button>
                 </div>
-
             </div>
         </>
     )
