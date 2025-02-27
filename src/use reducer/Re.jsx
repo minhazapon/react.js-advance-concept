fd1a7b5ad01f3c8936eb08e05340ea7cc470c558
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-26 21:35:51 -0800

    added file

--- src/use reducer/Re.jsx
+++ src/use reducer/Re.jsx
@@ -1,45 +1,59 @@
 
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 
 
 function Re() {
 
     const initialState = {
-        food: null
+        data: []
     }
 
     const reducer = (state, action) => {
-
-        if (action.type === " FETCH SUCCESS ") {
-
+        if (action.type === "FETCH SUCCESS") {
             return {
                 data: action.payload
             }
-
         } else if (action.type === "FETCH ERROR") {
-
             return {
-                data: null
+                data: []
             }
-
         } else {
             return state
         }
-
-
     }
-
 
     const [state, dispatch] = useReducer(reducer, initialState)
 
+    useEffect(() => {
 
+        fetch('https://raw.githubusercontent.com/minhazapon/react.js-advance-concept/refs/heads/main/public/re.json')
+            .then(res => res.json())
+            .then(data => {
+                dispatch({ type: "FETCH SUCCESS", payload: data })
+            })
+            .catch(() => {
+                dispatch({ type: "FETCH ERROR" })
+            })
 
+    }, [])
 
     return (
-        <div className=" mt-20 ">
-            <div>
-
-                <p> Data Show space </p>
+        <div className=" mt-20 flex justify-center ">
+            <div className=" grid  md:grid-cols-3 gap-5 ">
+                {
+                    state.data.map(state => <p key={state.id}>
+                        <div className="card bg-base-100 w-96 shadow-xl">
+                            <div className="card-body">
+                                <h2 className="card-title"> {state.name} </h2>
+                                <p> {state.price} </p>
+                                <p> {state.description} </p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </p>)
+                }
             </div>
         </div>
     )
