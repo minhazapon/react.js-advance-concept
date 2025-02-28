ef6dc0f8634f37ab166aebbb43be8bac63584afa
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-27 18:22:18 -0800

    added file

--- src/useCallBack/Call.jsx
+++ src/useCallBack/Call.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react"
+import ChildCall from "./ChildCall"
 
 
 function Call() {
 
+    const [inCe, setSetIn] = useState(0)
+
+    const handleIncrement = () => {
+
+        setSetIn(inCe + 1)
+
+    }
+
     return (
-        <div>
+        <div className="m-20">
+
+            <div>
+
+                <p className=" text-center text-xl font-serif "> {inCe} </p>
+                <div>
+                    <ChildCall onIncrement={handleIncrement} ></ChildCall>
+                </div>
+            </div>
 
         </div>
     )
--- src/useCallBack/ChildCall.jsx
+++ src/useCallBack/ChildCall.jsx
@@ -1,12 +1,15 @@
 
 
 
-import React from 'react'
+function ChildCall({ onIncrement }) {
 
-function ChildCall() {
+    console.log("child is re-creating")
+
     return (
         <div>
-
+            <div className=" flex justify-center ">
+                <button onClick={onIncrement} className=" btn w-[600px] bg-black text-cyan-400 border-[1px] border-cyan-400 ">Increment</button>
+            </div>
         </div>
     )
 }
